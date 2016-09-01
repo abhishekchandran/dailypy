d1a17ca89587907255c152bdc624966d4b4b6d10
Abhishek Chandran <abhishekchandran91@gmail.com>
2016-09-02 02:09:38 +0530

    scripts now satisfies all requirements

---
+++ configs.py
@@ -0,0 +1,31 @@
+min_capacity = 83
+
+hours = 2
+
+price = float(0.00)
+
+instances = {   "us-east": {
+			            "large": 0.12,
+			            "xlarge": 0.23,
+			            "2xlarge": 0.45,
+			            "4xlarge": 0.774,
+			            "8xlarge": 1.4,
+			            "10xlarge": 2.82
+			    },
+			    "us-west": {
+			            "large": 0.14,
+			            "2xlarge": 0.413,
+			            "4xlarge": 0.89,
+			            "8xlarge": 1.3,
+			            "10xlarge": 2.97
+			    },
+			}
+
+cpu_dict = {
+			'large' : 1,
+			'xlarge': 2,
+			'2xlarge' : 4,
+			'4xlarge' : 8,
+			'8xlarge' : 16,
+			'10xlarge': 32
+			}
--- res-alloc.py
+++ res-alloc.py
@@ -1,50 +1,46 @@
-from pandas import *
+import collections
+from configs import instances, cpu_dict, hours, price, min_capacity
 
-def KnapsackMax(value, weight, W, N):
-	K = [[0 for x in range(W+1)] for x in range(N+1)]
-
-	for i in range(N+1):
-		for w in range(W+1):
-			if(i==0 or w==0):
-				K[i][w] = 0
-			elif (weight[i-1] <= w):
-				K[i][w] = max(value[i-1]+K[i-1][w-weight[i-1]], K[i-1][w])
-			else: 
-				K[i][w] = K[i-1][w]
-	
-	#print DataFrame(K)
-	return K[N][W], getItems(K, value, weight, W, N)
-
-def KnapsackMin(value, weight, W, N):
-	K = [[0 for x in range(W+1)] for x in range(N+1)]
+def MinKnapsack(value, weight, W, N):
+	K = [[[0,[]] for x in range(W+1)] for x in range(N+1)]
 	for i in range(N+1):
 		for w in range(W+1):
 			if(w <= 0):
-				K[i][w] = 0
+				K[i][w][0] = 0
 			elif ((i<=0) and w>0):
-				K[i][w] = float('inf')
+				K[i][w][0] = float('inf')
 			elif ((i>0) and (w>0)):
-				K[i][w] = min(value[i-1]+K[i][w-weight[i-1]], K[i-1][w])
-	#print DataFrame(K)
-	return K[N][W], getItems(K, value, weight, W, N)
+				if ((weight[i-1]<=w) and (value[i-1]+K[i][w-weight[i-1]][0] <= K[i-1][w][0])):
+					K[i][w][0] = value[i-1]+K[i][w-weight[i-1]][0]
+					K[i][w][1] = K[i][w-weight[i-1]][1][:]
+					K[i][w][1].append(weight[i-1])
+				else:
+					K[i][w][0] = K[i-1][w][0]
+					K[i][w][1] = K[i-1][w][1][:]
+	return K[N][W]
 
-def getItems(K, value, weight, w, n):
-	bag = []
-	while(w > 0):
-		if (K[n][w] != K[n-1][w]):
-			bag.append(n)
-			n = n - 1
-			w = w - weight[n]
-		else:
-			n = n - 1
-	return bag
-
-value = [6, 10, 12]
-weight = [1, 2, 3]
-W = 5
-N = len(weight)
-
-print "#######################---MAX---#########################"
-print KnapsackMax(value, weight, W, N)
-print "#######################---MIN---#########################"
-print KnapsackMin(value, weight, W, N)
+def get_costs(instances, hours, cpus, price):
+	final_result = []
+	for key,val in instances.iteritems():
+		value, weight, count_keys = ([] for i in range(3))
+		info = {}
+		for i in val.values():
+			value.append(i)
+		
+		weight = [cpu_dict[x] for x in list(set(val.keys()) & set(cpu_dict.keys()))]
+		
+		result = MinKnapsack(value, weight, min_capacity, len(weight))		
+		
+		count_dict = collections.Counter(result[1])
+		for ki, valu in count_dict.iteritems():
+			for k,v in cpu_dict.iteritems():
+				if(ki == v):
+					count_keys.append(k)
+		count_values = [value for value in count_dict.values()]
+		info = {'region' : key, 'total_cost' : format(result[0]*hours,'.2f'), 'servers' : zip(count_keys, count_values)}
+		final_result.append(info)
+		
+	return sorted(final_result, key=lambda k:k['total_cost'])
+				
+print "#############--FINAL RESULT--###############"
+print get_costs(instances, hours, min_capacity, price)
